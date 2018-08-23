f4f1132f1182bf55233f84bb473eddb17f28b98c
prufeng <38185931+prufeng@users.noreply.github.com>
2018-08-23 01:36:48 +0000

    Create test.ts

---
+++ typescript/test.ts
@@ -0,0 +1,9 @@
+class Startup {
+    public static main(): number {
+        let date = new Date();
+        console.log('Hello World at ' + date.toLocaleString());
+        return 0;
+    }
+}
+
+Startup.main();  
