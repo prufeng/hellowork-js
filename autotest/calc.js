4380bae245f37d22d2d7d1e917327f54fd8b82a2
PanRufeng <prufeng@sina.com>
2018-10-24 20:38:19 +0800

    Move autotest to repo autotest-node, move tsexpress to repo tsexpress

--- autotest/calc.js
+++
@@ -1,8 +0,0 @@
-let calc = new Object();
-calc.add = function(x, y){
-    return x+y;
-}
-calc.minus = function(x,y){
-    return x-y;
-}
-module.exports = calc;
--- tsexpress/src/app/demo/userController.ts
+++
@@ -1,6 +0,0 @@
-import {Request, Response} from 'express';
-
-/* GET users listing. */
-export let list = (req: Request, res: Response) => {
-  res.send('respond with the user list here');
-};
