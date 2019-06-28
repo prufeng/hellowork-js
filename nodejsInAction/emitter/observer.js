465cb89b8548977822d5fa9c5ee1a9f086595544
Pan Rufeng <xingzhikong@126.com>
2019-06-28 16:47:19 +0800

    Update observer.js

--- nodejsInAction/emitter/observer.js
+++ nodejsInAction/emitter/observer.js
@@ -1,30 +1,50 @@
 var events = require('events');
 var net = require('net');
 var channel = new events.EventEmitter();
+channel.setMaxListeners(50);
 channel.clients = {};
 channel.subscriptions = {};
-channel.on('join', function(id, client) {
+channel.on('join', function (id, client) {
+    var welcome = "Welcome!\n"
+        + 'Guests online: ' + this.listeners('broadcast').length;
+    client.write(welcome + "\n");
     this.clients[id] = client;
-    this.subscriptions[id] = function(senderId, message) {
+    this.subscriptions[id] = function (senderId, message) {
         if (id != senderId) {
             this.clients[id].write(message);
         }
-    }
+    };
     this.on('broadcast', this.subscriptions[id]);
 });
+channel.on('leave', function (id) {
+    console.log('leave ' + id);
+    channel.removeListener('broadcast', this.subscriptions[id]);
+    channel.emit('broadcast', id, id + " has left the chat.\n");
+});
+channel.on('shutdown', function () {
+    channel.emit('broadcast', '', "Chat has shut down.\n");
+    channel.removeAllListeners('broadcast');
+});
+
 var server = net.createServer(function (client) {
     var id = client.remoteAddress + ':' + client.remotePort;
-    console.log(id);
-    client.on('connect', function() {
-        console.log('connected');
-        channel.emit('join', id, client);
-    });
-    client.on('data', function(data) {
+    // client.on('connect', function () {
+    console.log(id + ' connected');
+    channel.emit('join', id, client);
+    // });
+    client.on('data', function (data) {
         console.log(data.toString());
         data = data.toString();
+        if (data == "shutdown\r\n") {
+            channel.emit('shutdown');
+        }
         channel.emit('broadcast', id, data);
     });
+    client.on('close', function () {
+        console.log('close');
+        channel.emit('leave', id);
+    });
 });
-server.listen(8888, function(){
+server.listen(8888, function () {
     console.log('Server start listening on 8888');
 });
