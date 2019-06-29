6c40047360a5ec9d979b5e2155d08b98085b6fe7
Pan Rufeng <xingzhikong@126.com>
2019-06-29 16:48:19 +0800

    Add files via upload

---
+++ nodejsInAction/async/sequence/serial.js
@@ -0,0 +1,31 @@
+setTimeout(function () {
+    console.log('I execute first.');
+    setTimeout(function () {
+        console.log('I execute next.');
+        setTimeout(function () {
+            console.log('I execute last.');
+        }, 100);
+    }, 500);
+}, 1000);
+
+// var flow = require('nimble');
+// flow.series([
+//     function (callback) {
+//         setTimeout(function () {
+//             console.log('I execute first.');
+//             callback();
+//         }, 1000);
+//     },
+//     function (callback) {
+//         setTimeout(function () {
+//             console.log('I execute next.');
+//             callback();
+//         }, 500);
+//     },
+//     function (callback) {
+//         setTimeout(function () {
+//             console.log('I execute last.');
+//             callback();
+//         }, 100);
+//     }
+// ]);
