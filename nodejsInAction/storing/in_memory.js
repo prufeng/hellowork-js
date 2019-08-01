382b02019593e03ef825b8f31b2414c534857ddd
Pan Rufeng <xingzhikong@126.com>
2019-08-01 11:08:45 +0800

    Create in_memory.js

---
+++ nodejsInAction/storing/in_memory.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var counter = 0;
+var server = http.createServer(function (req, res) {
+    counter++;
+    res.write('I have been accessed ' + counter + ' times.');
+    console.log('I have been accessed ' + counter + ' times.');
+    res.end();
+}).listen(8888);
+
+//curl localhost:8888
