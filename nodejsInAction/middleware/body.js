3662546048c3ca7250cedccf0f7363c740d2de1a
Pan Rufeng <xingzhikong@126.com>
2020-04-19 16:36:06 +0800

    Update nodejsInAction/middleware - req.files

--- nodejsInAction/middleware/body.js
+++ nodejsInAction/middleware/body.js
@@ -1,15 +1,18 @@
 // curl localhost:3000 -d username=asdf
 // curl -d '{"username":"tobi"}' -H "Content-Type: application/json" localhost:3000
-
-
+// curl localhost:3000 -F image=unins000.msg -F name=tobi
 var connect = require('connect');
 var bodyParser = require('body-parser');
+var multipart = require('connect-multiparty')();
 var app = connect()
     .use(bodyParser.urlencoded({extended: false}))
     .use(bodyParser.json())
+    .use('/upload', multipart, (req, res) => {
+        console.log(req.files); //undefined
+    })
     .use(function (req, res) {
         // .. do stuff to register the user ..
-        // console.log(req.body)
+        console.log(req.body);
         res.end('Registered new user: ' + req.body.username);
     });
 
